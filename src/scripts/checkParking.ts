db00481ae7855d0290e9259fac025ad31e96b3c1
Kizito Mrema <kizomanizo@gmail.com>
2025-07-09 14:37:52 +0300

    FIX: Change no bill info color from warning to info

--- src/scripts/checkParking.ts
+++ src/scripts/checkParking.ts
@@ -3,36 +3,6 @@ import showAlert from '@/scripts/showAlert'
 import { useLoading, useResponse, usePath, useHistory } from '@/stores/store'
 import router from '@/router' // Import the router instance from the router file
 
-// const tzRegex = /^[Tt]\d{3}[A-Za-z]{3}$/
-
-// export const handleCheckParking = async (regNumber: any) => {
-//   if (useLoading.visible) return
-//   useLoading.toggleVisibility(true)
-//   try {
-//     if (tzRegex.test(regNumber)) {
-//       const termisUrl = import.meta.env.VITE_TERMIS_ENDPOINT
-//       const termisResponse = await axios.get(termisUrl + regNumber, {})
-//       // Store the token in Vue store
-//       const response = termisResponse.data
-//       if (!response.status) {
-//         showAlert('alert-warning', 'WARNING', `${regNumber} has no bills!!`)
-//         return
-//       }
-//       // Add the REG to the history
-//       useHistory.setHistory(regNumber)
-//       useResponse.setResponse(JSON.stringify(response.data))
-//       usePath.changeName('results')
-//       await router.push('/results')
-//     } else {
-//       showAlert('alert-danger', 'Error', `${regNumber || 'EMPTY'} is an invalid plate number`)
-//     }
-//   } catch (error: any) {
-//     console.error(error)
-//   } finally {
-//     useLoading.toggleVisibility(false)
-//   }
-// }
-
 export const handleCheckParking = async (regNumber: any) => {
   if (useLoading.visible) return
   useLoading.toggleVisibility(true)
@@ -64,7 +34,7 @@ export const handleCheckParking = async (regNumber: any) => {
       // Store the token in Vue store
       const response = termisResponse.data
       if (!response.status) {
-        showAlert('alert-warning', 'WARNING', `${sanitizedRegNumber} has no bills!!`)
+        showAlert('alert-info', 'INFO', `${sanitizedRegNumber} has no bills!!`)
         return
       }
 
